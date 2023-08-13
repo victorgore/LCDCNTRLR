d853b1e00e79ca66db0d6a7f8338ef407a6df29b
Victor Gore <64615476+victorgore@users.noreply.github.com>
2023-08-13 00:20:31 -0600

    Update MainController.java
    Changed led delay to 3 seconds, added 4 blinks when no input is found

--- MainController.java
+++ MainController.java
@@ -4,20 +4,34 @@ public class MainController {
         LedController ledController = new LedController();
         LcdController lcdController = new LcdController();
 
+        boolean hasInput = false;
+        int blinkCount = 0;
+        int maxBlinkCount = 4;
+
         while (true) {
             // Check power status
             boolean hasPower = true; // Replace with actual power status detection
             ledController.setColor(hasPower);
 
             // Check input status
-            boolean hasInput = false; // Replace with actual input status detection
+            hasInput = false; // Replace with actual input status detection
+
             if (hasInput) {
                 lcdController.displayInput();
+                blinkCount = 0; // Reset blink count if input is detected
             } else {
-                lcdController.displayNoInput();
+                if (blinkCount < maxBlinkCount) {
+                    ledController.turnOn();
+                    TimeUnit.MILLISECONDS.sleep(500); // LED on for 500 milliseconds
+                    ledController.turnOff();
+                    TimeUnit.MILLISECONDS.sleep(500); // LED off for 500 milliseconds
+                    blinkCount++;
+                } else {
+                    lcdController.displayNoInput();
+                }
             }
 
-            TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(3);
         }
     }
 }
