b9092bbbc2bce2978ff12afe684857e33baa8dde
Victor Gore <64615476+victorgore@users.noreply.github.com>
2023-08-15 19:15:03 -0600

    Added detection for the low power mode
    Acts accordingly when low power is detected

--- MainController.java
+++ MainController.java
@@ -16,11 +16,16 @@ public class MainController {
             // Check input status
             hasInput = false; // Replace with actual input status detection
 
+            // Check low power status
+            boolean lowPower = checkLowPowerStatus(); // Replace with your low power detection logic
+
             if (hasInput) {
                 lcdController.displayInput();
                 blinkCount = 0; // Reset blink count if input is detected
             } else {
-                if (blinkCount < maxBlinkCount) {
+                if (lowPower) {
+                    ledController.setColor(false, true); // LED in yellow low power mode
+                } else if (blinkCount < maxBlinkCount) {
                     ledController.turnOn();
                     TimeUnit.MILLISECONDS.sleep(500); // LED on for 500 milliseconds
                     ledController.turnOff();
@@ -33,5 +38,11 @@ public class MainController {
 
             TimeUnit.SECONDS.sleep(3);
         }
+    }
+
+    // Replace this method with your actual low power detection logic
+    private static boolean checkLowPowerStatus() {
+        // Replace with code to check if the system is in low power mode
+        return false; // For demonstration purposes, assuming not in low power mode
     }
 }
